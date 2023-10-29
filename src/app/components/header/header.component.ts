e497079ab2fa8fcada208a4c286951fdab6192a0
qtambone <quintinobello91@gmail.com>
2023-10-29 11:52:45 +0100

    search no result

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Film } from 'src/app/modele/film.modele';
 import { ApiService } from 'src/app/services/api/api.service';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { SearchTitleService } from 'src/app/services/search-title/search-title.service';
 
 
@@ -12,7 +12,6 @@ import { SearchTitleService } from 'src/app/services/search-title/search-title.s
 })
 
 export class HeaderComponent {
-  films : Film[] = [] ;
   query: string = ''; 
 
   constructor(private api: ApiService, private router: Router, private searchTitleService: SearchTitleService) {}
@@ -27,4 +26,5 @@ export class HeaderComponent {
     //router vers les resultats --> search page component
     this.router.navigateByUrl("search");
   }
+
 }
