8e8f0ee0d2c08b898def7dadda6a9374d43b3f4e
hugo.albert.marques@gmail.com <hugo.albert.marques@gmail.com>
2023-10-30 19:38:32 +0100

    selectedMovies

--- src/app/service/home-page-data.service.ts
+++ src/app/service/home-page-data.service.ts
@@ -7,11 +7,30 @@ import {BehaviorSubject} from "rxjs";
 })
 @Injectable()
 export class HomePageDataService {
-  private selectedDataSubject = new BehaviorSubject<any>(null);
+  private selectedMovies : Film[] =[];
+  private selectedDataSubject  = new BehaviorSubject<Film>({
+    adult: false,
+    backdrop_path: "",
+    genres: [],
+    id: 0,
+    imdb_id: "",
+    overview: "",
+    popularity: 0,
+    poster_path: "",
+    release_date: "",
+    runtime: 0,
+    tagline: "",
+    title: "",
+    vote_average: 0,
+    vote_count: 0
+  });
   selectedData$ = this.selectedDataSubject.asObservable();
 
   constructor() {}
-  setSelectedData(data: any) {
+
+  setSelectedData(data: Film) {
     this.selectedDataSubject.next(data);
+    this.selectedMovies.push(data)
+    console.log(this.selectedMovies)
   }
 }
